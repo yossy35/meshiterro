af8eb8a89fa3889aefe0f69fd707d01c644a4f69
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-02-08 05:48:49 +0000

    [Finish]構成の改善

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,13 @@ Rails.application.routes.draw do
     resources :users, only: [:destroy]
   end
 
-  root to: 'homes#top'
-  devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  get 'homes/about' => 'homes#about', as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
-  resources :users, only: [:show, :edit, :update]
-end
